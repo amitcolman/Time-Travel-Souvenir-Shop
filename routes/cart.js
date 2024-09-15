a46ead6db6c6459fc80ad73e289dad68d99933fb
lior-le <lior0076@gmail.com>
2024-09-15 22:57:59 +0300

    added cart functionality (and fixed minor error in itemController)

---
+++ routes/cart.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const cartController = require('../src/controllers/cartController');
+const authUserVerifier = require('../src/controllers/authUserVerifier');
+
+// Routes
+router.post('/add', authUserVerifier, cartController.addToCart);
+router.delete('/remove', authUserVerifier,  cartController.removeFromCart)
+router.get('/get', authUserVerifier, cartController.getCart)
+
+module.exports = router;
---
+++ src/controllers/cartController.js
@@ -0,0 +1,50 @@
+const itemModel = require('../models/itemModel');
+const userModel = require('../models/userModel');
+
+const cartController = {
+    async addToCart(req, res) {
+        // Verify that the Item exists.
+        let itemName = req.body.itemName
+        const item = await itemModel.findOne({itemName: itemName});
+        if (!item) {
+            res.status(404).send({status: 'Error', message: 'Item not found'});
+            return;
+        }
+        if (item.quantity === 0){
+            res.status(410).send({status: 'Error', message: 'Item not in stock'});
+            return;
+        }
+        let username = req.session.user.username;
+        let cartItem = {cart: itemName}
+        const user = await userModel.findOneAndUpdate({ username: username }, { $push: cartItem }, {new: true});
+        if (!user) {
+            res.status(404).send({status: 'Error', message: 'User not found'});
+            return;
+        }
+        res.status(201).send({status: 'Success', message: 'Item added to cart'});
+    },
+
+    async removeFromCart(req, res) {
+        let itemName = req.body.itemName
+        let username = req.session.user.username;
+        let cartItem = {cart: itemName};
+        const user = await userModel.findOneAndUpdate({ username: username }, { $pull: cartItem }, {new: true});
+        if (!user) {
+            res.status(404).send({status: 'Error', message: 'User not found'});
+            return;
+        }
+        res.status(201).send({status: 'Success', message: 'Item removed from cart'});
+    },
+    async getCart(req, res) {
+        let username = req.session.user.username;
+        const user = await userModel.findOne({ username: username })
+        if (!user) {
+            res.status(404).send({status: 'Error', message: 'User not found'});
+            return;
+        }
+
+        res.status(200).send({status: 'Success', cart: user.cart});
+    }
+}
+
+module.exports = cartController;
