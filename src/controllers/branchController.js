ef72752a4dd456d075a641feb7c7bd8ac8138602
amitcolman <amity88g@cs.colman.ac.il>
2024-09-16 18:37:18 +0300

    - Added some routes for branches

--- src/controllers/branchController.js
+++ src/controllers/branchController.js
@@ -4,6 +4,50 @@ const branchController = {
     async listBranches(req, res) {
         const branches = await branchModel.find();
         res.status(200).send({status: 'Success', branches: branches});
+    },
+
+    async createBranch(req, res) {
+        let name = req.body.name;
+        let address = req.body.address;
+
+        let branch = new branchModel({
+            name: name,
+            address: address
+        });
+
+        await branch.save().then(() => {
+            res.status(201).send({status: 'Success', message: 'Branch created'});
+        }).catch((error) => {
+            console.error('Error creating branch:', error);
+            res.status(500).send({status: 'Error', message: 'Error creating branch'});
+        });
+    },
+
+    async deleteBranch(req, res) {
+        const branch = await branchModel.findOneAndDelete({name: req.body.name});
+        if (!branch) {
+            res.status(404).send({status: 'Error', message: 'Branch not found'});
+            return;
+        }
+        res.status(200).send({status: 'Success', branch: branch});
+    },
+
+    async updateBranch(req, res) {
+        try {
+            const branch = await branchModel.findOneAndUpdate(
+                {name: req.query.name},
+                {address: req.body.address},
+                {new: true}
+            );
+            if (!branch) {
+                res.status(404).send({status: 'Error', message: 'Branch not found'});
+                return;
+            }
+            res.status(200).send({status: 'Success', branch: branch});
+        } catch (error) {
+            console.error('Error updating branch:', error);
+            res.status(500).send({status: 'Error', message: 'Error updating branch'});
+        }
     }
 }
 
